337ee46efd3597a070878291446ea6da3a1bbc74
langlanglang <jinren.lang@gmail.com>
2020-06-02 17:09:58 +0800

    update ajax.js

--- src/api/base.js
+++ src/api/base.js
@@ -1,13 +1,6 @@
-// var activity_api = 'http://dev.service.vcinema.cn:8019/'
 var activity_api = '/lang/'
-var phone_api = 'http://dev.api.vcinema.cn/phone/v5.0/'
-var seed_url = 'http://dev.web.vcinema.cn:3088/doras_h5/app/seed-detail/index.html'
-// var activity_api = ''https://shop.api.vcinema.cn/'
-// var phone_api = 'https://p.open.api.vcinema.cn/v5.0/'
-// var seed_url = 'https://h5.vcinema.cn/h5/app/seed-detail/index.html'
-
+var phone_api = 'http://www.baidu.com'
 export default {
   activity_api: activity_api,
-  phone_api: phone_api,
-  seed_url: seed_url
+  phone_api: phone_api
 }
